84332192b22c899d055e44a6cf886d565492c383
dishadi147-cloud <dishadi147@gmail.com>
2026-04-29 15:42:34 +0400

    6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,38 @@
-﻿using ConsoleApp1;
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp1;
 
-string title = "Bohemian Rhapsody";
-string artist = "Queen";
-string album = "A Night at the Opera";
-int durationSeconds = 355;
-string genre = "Rock";
-int bitrate = 320;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string title = "Bohemian Rhapsody";
+        string artist = "Queen";
+        string album = "A Night at the Opera";
+        int durationSeconds = 355;
+        string genre = "Rock";
+        int bitrate = 320;
 
-Song s = new Song(title, artist, album, durationSeconds, genre, bitrate);
-s.print();
-s.Play();
-s.Pause();
-s.GetQualityInfo();
+        Song s = new Song(title, "Queen", 1975, artist, album, durationSeconds, genre, bitrate);
+        s.print();
+        s.Play();
+        s.Pause();
+        s.GetQualityInfo();
+
+        Console.WriteLine("\n" + new string('=', 50) + "\n");
+
+        List<MediaItem> mediaLibrary = new List<MediaItem>();
+
+        mediaLibrary.Add(new Song("Bohemian Rhapsody", "Queen", 1975, "Freddie Mercury", "A Night at the Opera", 355, "Rock", 320));
+        mediaLibrary.Add(new Movie("Inception", "Christopher Nolan", 2010, "Christopher Nolan", 8.8));
+        mediaLibrary.Add(new Podcast("Tech Talk", "John Doe", 2023, "Jane Smith", 42));
+
+
+        foreach (MediaItem item in mediaLibrary)
+        {
+            item.print();
+            item.Play();
+            Console.WriteLine();
+        }
+    }
+}
