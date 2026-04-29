56895fd55d0d07264771d8f452e7fa0b9a37dde2
dishadi147-cloud <dishadi147@gmail.com>
2026-04-29 15:53:01 +0400

    7

---
+++ ConsoleApp1/ConsoleApp1/Library.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibrarySystem
+{
+    public class Library<T> where T : LibraryItem
+    {
+        private List<T> _items = new List<T>();
+
+        public Library(IEnumerable<T>? items = null)
+        {
+            if (items != null)
+                _items.AddRange(items);
+        }
+
+        public void AddItem(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void BorrowItem(T item)
+        {
+            item.Borrow();
+        }
+
+        public List<T> GetAvailableItems()
+        {
+            return _items.Where(item => item.IsAvailable).ToList();
+        }
+
+        public void PrintAllItems()
+        {
+            Console.WriteLine($"Всего создано предметов: {LibraryItem.TotalCount}");
+            Console.WriteLine("Список предметов в библиотеке:");
+            foreach (var item in _items)
+            {
+                item.PrintInfo();
+            }
+        }
+
+        public T? FindById(int id)
+        {
+            return _items.FirstOrDefault(item => item.Id == id);
+        }
+    }
+}
