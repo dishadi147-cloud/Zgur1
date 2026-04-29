25cb66e3eb72017b06d3b361183d724245ce9265
dishadi147-cloud <dishadi147@gmail.com>
2026-04-29 15:53:45 +0400

    8

---
+++ ConsoleApp1/ConsoleApp1/PhysicalBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LibrarySystem
+{
+    public class PhysicalBook : LibraryItem
+    {
+        public string Publisher { get; set; }
+        public int Pages { get; set; }
+
+        public PhysicalBook(int id, DateTime dateReceived, string publisher, int pages)
+            : base(id, dateReceived)
+        {
+            Publisher = publisher;
+            Pages = pages;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Физическая книга: ID={Id}, Издательство={Publisher}, Страниц={Pages}, Доступна={IsAvailable}");
+        }
+    }
+}
