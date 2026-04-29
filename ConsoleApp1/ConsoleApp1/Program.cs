a92602767d2d52d8233dc3c1f684faddc58b97dd
dishadi147-cloud <dishadi147@gmail.com>
2026-04-29 15:54:55 +0400

    9

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,38 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using ConsoleApp1;
+﻿using LibrarySystem;
 
-class Program
+
+PhysicalBook book1 = new PhysicalBook(1, DateTime.Parse("2024-01-15"), "Просвещение", 350);
+PhysicalBook book2 = new PhysicalBook(2, DateTime.Parse("2024-02-20"), "Эксмо", 280);
+EBook ebook1 = new EBook(3, DateTime.Parse("2024-03-10"), "PDF", 5.2);
+EBook ebook2 = new EBook(4, DateTime.Parse("2024-03-15"), "EPUB", 3.8);
+
+
+LibraryItem[] items = new LibraryItem[] { book1, book2, ebook1 };
+Library<LibraryItem> library = new Library<LibraryItem>(items);
+
+
+library.PrintAllItems();
+
+
+LibraryItem? found = library.FindById(2);
+if (found != null)
 {
-    static void Main(string[] args)
-    {
-        string title = "Bohemian Rhapsody";
-        string artist = "Queen";
-        string album = "A Night at the Opera";
-        int durationSeconds = 355;
-        string genre = "Rock";
-        int bitrate = 320;
-
-        Song s = new Song(title, "Queen", 1975, artist, album, durationSeconds, genre, bitrate);
-        s.print();
-        s.Play();
-        s.Pause();
-        s.GetQualityInfo();
-
-        Console.WriteLine("\n" + new string('=', 50) + "\n");
-
-        List<MediaItem> mediaLibrary = new List<MediaItem>();
-
-        mediaLibrary.Add(new Song("Bohemian Rhapsody", "Queen", 1975, "Freddie Mercury", "A Night at the Opera", 355, "Rock", 320));
-        mediaLibrary.Add(new Movie("Inception", "Christopher Nolan", 2010, "Christopher Nolan", 8.8));
-        mediaLibrary.Add(new Podcast("Tech Talk", "John Doe", 2023, "Jane Smith", 42));
-
-
-        foreach (MediaItem item in mediaLibrary)
-        {
-            item.print();
-            item.Play();
-            Console.WriteLine();
-        }
-    }
+    Console.WriteLine($"\nНайден предмет с ID=2:");
+    found.PrintInfo();
 }
+else
+{
+    Console.WriteLine("Не найден");
+}
+
+
+Console.WriteLine("\nВыдача книги ID=1:");
+library.BorrowItem(book1);
+
+
+Console.WriteLine("\nДоступные предметы:");
+List<LibraryItem> available = library.GetAvailableItems();
+foreach (var item in available)
+{
+    item.PrintInfo();
+}
+
+
+Console.WriteLine("\nВозврат книги ID=1:");
+book1.Return();
+
+Console.WriteLine("\nПосле добавления ещё одного предмета:");
+EBook ebook3 = new EBook(5, DateTime.Now, "MOBI", 4.5);
+library.AddItem(ebook3);
+library.PrintAllItems();
